b33d69018b4d511df7c4956d8f963f5387117285
=Stian Liknes <stianlik@gmail.com>
2015-04-12 12:23:52 +0200

    Spaces ExampleTest

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,11 +4,11 @@ class ExampleTest extends PHPUnit_Framework_TestCase
 {
     public function test_plugin_shouldExist()
     {
-		$this->assertTrue(elgg_plugin_exists('elgg-phpunit'));
+        $this->assertTrue(elgg_plugin_exists('elgg-phpunit'));
     }
-	
-	public function test_config_shouldBeEnrichedByTestSetup()
-	{
-		$this->assertTrue(elgg_get_config('phpunit'));
-	}
+    
+    public function test_config_shouldBeEnrichedByTestSetup()
+    {
+        $this->assertTrue(elgg_get_config('phpunit'));
+    }
 }
